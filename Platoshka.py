f5ed2964aa21bbe79a3bbf596c99966739c7e2fe
P1atoshka <platon.godik@gmail.com>
2024-12-25 21:21:04 +0700

    FirstCommoit
    ЕЕЕЕе я платон!

---
+++ Platoshka.py
@@ -0,0 +1,29 @@
+#class singleton: 
+#   _instants= None 
+#     def __new__(cls):
+#         if cls._instants is None:
+#            cls._instants = super(singleton,cls).__new__(cls)
+#   return cls._instants
+                                                      
+
+        
+print('3 '' 4') 
+
+
+
+
+
+
+
+
+
+
+
+
+#from sys import getsizeof
+#print ( 'Ответ', getsizeof (3**9090001) / (1024*1024))
+#Swag = 52
+
+
+
+
