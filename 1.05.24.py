2b1411577eee90b712ecafaa4ca4e7b1ec5ade9b
LifeCoreLoop <59015195+LifeCoreLoopExe@users.noreply.github.com>
2025-05-14 23:33:41 +0900

    Fix

--- 1.05.24.py
+++ 1.05.24.py
@@ -1,15 +1,14 @@
 def is_valid_card_number(card_number):
-
-    return card_number.isdigit() and len(card_number) == 12
-
+    card_number = card_number.replace(" ", "")
+    return card_number.isdigit() and len(card_number) == 16
 def is_all_uppercase(name):
     
-    return name.isupper()
+    return name.isupper() and name.isalpha()   
 
 def main():
-    card_number = input("Введите номер карты (12 цифр): ")
-    first_name = input("Введите имя владельца карты: ")
-    last_name = input("Введите фамилию владельца карты: ")
+    card_number = input("Введите номер карты (16 цифр): ").strip()
+    first_name = input("Введите имя владельца карты: ").strip()
+    last_name = input("Введите фамилию владельца карты: ").strip()
     if is_valid_card_number(card_number):
         print("Номер карты введен правильно.")
     else:
---
+++ 1.05.24UI.py
@@ -0,0 +1,7 @@
+imp
+#def valide():
+
+root = tk
+root.title("Валидация карты")
+
+tk.Label(root,text = "Номер Карты:").pack()
