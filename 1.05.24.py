b5244e674dc00cae5468320f1936846030831d49
P1atoshka <platon.godik@gmail.com>
2025-05-22 21:06:09 +0700

    sd
    уца

--- 1.05.24.py
+++ 1.05.24.py
@@ -1,19 +1,19 @@
 def is_valid_card_number(card_number):
 
-    return card_number.isdigit() and len(card_number) == 12
+    return card_number.isdigit() and len(card_number) == 16
 
 def is_all_uppercase(name):
     
     return name.isupper()
 
 def main():
-    card_number = input("Введите номер карты (12 цифр): ")
+    card_number = input("Введите номер карты (16 цифр): ")
     first_name = input("Введите имя владельца карты: ")
     last_name = input("Введите фамилию владельца карты: ")
     if is_valid_card_number(card_number):
         print("Номер карты введен правильно.")
     else:
-        print("Ошибка: номер карты должен состоять из 12 цифр.")
+        print("Ошибка: номер карты должен состоять из 16 цифр.")
 
     if is_all_uppercase(first_name):
         print("Имя введено правильно (все буквы заглавные).")
